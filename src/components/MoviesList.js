ecc73e28a40b4049256af47ef9e1b44ecb220989
Vipulag12 <97009659+Vipulag12@users.noreply.github.com>
2022-12-24 10:44:07 +0530

    Update MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,33 +1,67 @@
 import React from "react";
 
+import { MovieContext } from "./MovieContext";
+
 class MoviesList extends React.Component {
 
-  render() {
-    
-    // Get movies list using MoviesContext.
+static contextType = MovieContext;
 
-    return (
-      <div id="movies-table">
-        <style>{
-        `
-        td { padding : 20px; }
-        tr:hover { background: #3374C2}
-        `}</style>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-            </tr>
-          </thead>
-          <tbody>
-              <tr id="table-row">
-                <td>Movie Title</td>
-              </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+render() {
+
+const {movies, setState} = this.context;
+
+const handleOnClick = (id) =>{setState((state) => ({...state, selectedMovieId: id }));
+
+}
+
+return (
+
+<div id="movies-table">
+
+<style>{
+
+`
+
+td { padding : 20px; }
+
+tr:hover { background: #3374C2}
+
+`}</style>
+
+<table>
+
+<thead>
+
+<tr>
+
+<th>Title</th>
+
+</tr>
+
+</thead>
+
+<tbody>
+
+{movies.map((movie) => (
+
+<tr id={`table-row-${movie.id}`} key={movie.id} onClick={() => handleOnClick(movie.id)}>
+
+<td>{movie.title}</td>
+
+</tr>
+
+))}
+
+</tbody>
+
+</table>
+
+</div>
+
+);
+
+}
+
 }
 
 export default MoviesList;
