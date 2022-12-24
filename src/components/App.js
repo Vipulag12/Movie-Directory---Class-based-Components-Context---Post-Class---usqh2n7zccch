719a416e4bd9e4861a6a3c62544321ae6b60b7bb
Vipulag12 <97009659+Vipulag12@users.noreply.github.com>
2022-12-24 10:42:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,39 @@ const App = () => {
   
   return (
     <div id="main">
-      <h1>Movie List</h1>
+      <h1>import React from "react";
+
+import "../styles/App.css";
+
+import MovieProvider from "./MovieContext";
+
+import Movie from "./Movie";
+
+import MoviesList from "./MoviesList";
+
+const App = () => {
+
+return (
+
+<MovieProvider>
+
+<div id="main">
+
+<h1>Movie List</h1>
+
+<MoviesList />
+
+<Movie />
+
+</div>
+
+</MovieProvider>
+
+);
+
+};
+
+export default App;Movie List</h1>
       <MoviesList />
       <Movie />
     </div>
